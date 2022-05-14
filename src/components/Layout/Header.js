c66d842066be45998e56da1c643daf91322d738d
samy884 <ssgill884@gmail.com>
2022-05-14 21:41:14 +0530

    adding image

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,19 @@
-import {Fragment} from 'react';
+import { Fragment } from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
 
 const Header = () => {
-    return <Fragment>
-    <header>
-    <h1>ReactMeal</h1>
-    <button>Cart</button>
-    </header>
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeal</h1>
+        <button>Cart</button>
+      </header>
+      <div>
+        <img src={mealsImage} />
+      </div>
     </Fragment>
-}
+  );
+};
 
 export default Header;
