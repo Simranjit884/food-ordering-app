872b7512ccff0c00fcf619f2b711eb836a852314
samy884 <ssgill884@gmail.com>
2022-05-14 21:01:34 +0530

    creating header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,12 @@
+import {Fragment} from 'react';
+
+const Header = () => {
+    return <Fragment>
+    <header>
+    <h1>ReactMeal</h1>
+    <button>Cart</button>
+    </header>
+    </Fragment>
+}
+
+export default Header;
