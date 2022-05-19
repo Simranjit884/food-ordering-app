2296c6edc257d9b96baab6d143c3b10eca75fa57
samy884 <ssgill884@gmail.com>
2022-05-19 20:29:09 +0530

    showing modal on button click

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
 import HeaderCardButton from "./HeaderCardButton";
 
-const Header = () => {
+const Header = props => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeal</h1>
-        <HeaderCardButton />
+        <HeaderCardButton onClick={props.onShow}/>
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food" />
