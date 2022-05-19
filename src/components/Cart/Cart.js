60721edd52e3427d4d581b84523335c25826ffb8
samy884 <ssgill884@gmail.com>
2022-05-19 10:58:55 +0530

    creating card item

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,25 @@
 const Cart = (props) => {
-  return <div></div>;
+  const cartitems = (
+    <ul>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      cartitems
+      <div>
+        <span>Total Amount</span>
+        <span>35.65</span>
+      </div>
+      <div>
+        <button>Close</button>
+        <button>Order</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
