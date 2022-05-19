26c602360184fbadc9392c434ebb410d9b1db236
samy884 <ssgill884@gmail.com>
2022-05-19 19:14:24 +0530

    creating modal componenet

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,16 @@
+const BackDrop = (props) => {
+  return <div></div>;
+};
+const OverLay = (props) => {
+  return (
+    <div>
+      <div>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  return <div></div>;
+};
+
+export default Modal;
