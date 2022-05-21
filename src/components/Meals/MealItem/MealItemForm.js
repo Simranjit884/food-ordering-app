ebf83aef41376ee3e26374c930ebf98dc372bc61
samy884 <ssgill884@gmail.com>
2022-05-21 22:16:45 +0530

    validating enteredAmount

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,17 +1,35 @@
-import classes from './MealItemForm.module.css';
-import Input from '../../UI/Input';
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
+import { useRef } from "react";
 
 const MealItemForm = (props) => {
+  // const cartInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = cartInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber.length < 1 ||
+      enteredAmountNumber > 5
+    )
+      return;
+  };
   return (
-    <form className={classes.form}>
-      <Input label="Amount" input={{
-          id: 'amount_' + props.id,
-          type: 'number',
-          min: '1',
-          max: '5',
-          step: '1',
-          defaultValue: '1'
-      }}/>
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
     </form>
   );
