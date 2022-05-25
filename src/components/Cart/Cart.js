feb60bc04b2e0bcc9ed80adb64d9b395a6ea7981
samy884 <ssgill884@gmail.com>
2022-05-25 12:06:17 +0530

    updating total amount

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,12 @@
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartContext from '../../Store/cart-context';
+import { useContext } from "react/cjs/react.production.min";
 
 const Cart = (props) => {
+  const cartCntxt = useContext(CartContext);
+  const totalAmount = `$${cartCntxt.totalAmount}`;
+
   const cartitems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
@@ -15,7 +20,7 @@ const Cart = (props) => {
       {cartitems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.65</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
