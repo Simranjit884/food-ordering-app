d2e78c33023796297a71c551a90ab456573ba798
samy884 <ssgill884@gmail.com>
2022-06-12 11:33:26 +0530

    using loading class

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -59,7 +59,7 @@ const AvailableMeals = () => {
 
   if (isLoading) {
     return (
-      <section>
+      <section className={classes.mealsLoading}>
         <p>Loading...</p>
       </section>
     );
