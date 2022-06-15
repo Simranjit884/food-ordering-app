c39d2bb9e391c2e7ddd4df2baba84adb7fddb3fb
samy884 <ssgill884@gmail.com>
2022-06-15 10:01:27 +0530

    defining input refs

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./Checkout.module.css";
 
 const isEmpty = (value) => value.trim() === "";
@@ -51,19 +51,23 @@ const Checkout = (props) => {
     <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input ref={nameInputRef} type="text" id="name" />
+        {!formInputValidity.name && <p>Please enter a valid name</p>}
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street" />
+        <input ref={streetInputRef} type="text" id="street" />
+        {!formInputValidity.street && <p>Please enter a valid street</p>}
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" />
+        <input ref={postalCodeRef} type="text" id="postal" />
+        {!formInputValidity.postalCode && <p>Please enter a valid Postal code(Atleast five input char.)</p>}
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+        <input ref={cityInputRef} type="text" id="city" />
+        {!formInputValidity.city && <p>Please enter a valid city.</p>}
       </div>
       <div className={classes.actions}>
         <button type="button" onClick={props.onCancel}>
